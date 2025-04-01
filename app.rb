d368d7727c693e28177c83b2c9683d374d3aa829
Kyl Justin Perez <perezkyljustin@gmail.com>
2025-04-01 23:26:34 +0800

    Add basic APIs of the app

--- app.rb
+++ app.rb
@@ -5,6 +5,29 @@ require 'json'
 set :port, ENV.fetch('PORT', 3000)
 set :bind, '0.0.0.0'
 
+MEOWS = ["Meow! 🐱", "Mrrrow! 😺", "Purr... 💤", "Mew! 😻"]
+
+get '/meow' do
+  content_type :json
+  { sound: MEOWS.sample }.to_json
+end
+
+get '/cat' do
+  content_type :json
+  uri = URI('https://api.thecatapi.com/v1/images/search')
+  response = Net::HTTP.get(uri)
+  image = JSON.parse(response).dig(0, 'url')
+  { image: image }.to_json
+end
+
+get '/fact' do
+  content_type :json
+  uri = URI('https://catfact.ninja/fact')
+  response = Net::HTTP.get(uri)
+  fact = JSON.parse(response)['fact']
+  { fact: fact }.to_json
+end
+
 get '/' do
-    "<h1>Hello World! ~ From Meoworld</h1>"
+  "<h1>Welcome to Cat-as-a-Service! 🐱</h1><p>Try <code>/meow</code>, <code>/cat</code>, or <code>/fact</code>!</p>"
 end
